1bb805436f697b66f409062a5bb55f70d1b7e1ad
Rowan <rkortlandt@gmail.com>
2023-08-16 15:18:12 -0400

    deletion

--- src/hooks/useLeaveFamily.tsx
+++
@@ -1,9 +0,0 @@
-import { useMutation } from '@tanstack/react-query';
-import pb from '../contexts/pocketbase';
-
-export function useLeaveFamily() {
-    const leaveFamily = async () => {
-      await pb.collection('familyRelation').delete(pb.authStore.model?.familyRelation);
-    };
-    return useMutation(leaveFamily);
-  }
